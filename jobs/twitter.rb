d644b2d201d6cb551a3db37a1133a6411be07462
Markus-Be <markus@terminal21.de>
2015-04-29 11:30:01 +0200

    first test

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -3,28 +3,26 @@ require 'twitter'
 
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
-Twitter.configure do |config|
-  config.consumer_key = '6V3etTdl8tpOKI2jI4tY5L7Zm'
-  config.consumer_secret = 'UwSMkXWVyk8AfoH8Mn9JV3REAYsNqcROU1SxBL5SR07UvPOWpb'
-#  config.oauth_token = 'terminal_21'
-#  config.oauth_token_secret = '81910378'
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = 'YOUR_CONSUMER_KEY'
+  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+  config.oauth_token = 'YOUR_OAUTH_TOKEN'
+  config.oauth_token_secret = 'YOUR_OAUTH_SECRET'
 end
 
-search_term = URI::encode('#cccamp')
+search_term = URI::encode('#todayilearned')
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
-    tweets = Twitter.search("#{search_term}").results
+    tweets = twitter.search("#{search_term}")
 
     if tweets
-      tweets.map! do |tweet|
+      tweets = tweets.map do |tweet|
         { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
       end
       send_event('twitter_mentions', comments: tweets)
     end
-#  rescue Twitter::Error
-  rescue Twitter::Error => e
-    puts "Twitter Error: #{e}"
+  rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
   end
 end
